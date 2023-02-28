2ebd8ceaf195d1c158343e0d454318f48da9bd31
leandrogbs <leandrosendra09@gmail.com>
2023-02-28 01:44:56 -0300

    bugs solved

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ProductProvider from './contexts/ProductContext';
+// sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
+// cart provider
+import CartProvider from './contexts/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SidebarProvider>
+  <CartProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </CartProvider>
+</SidebarProvider>,
 )
